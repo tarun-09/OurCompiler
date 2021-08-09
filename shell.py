ab61f960b925755fd6a4eac1941075aea26055ad
Neela <neelamohankottavalasa@gmail.com>
2021-08-09 20:14:31 +0530

    Merge remote-tracking branch 'origin/mohan' into mohan
    # Conflicts:
    #	Sans.py
    #	shell.p
    modules

---
+++ shell.py
@@ -0,0 +1,25 @@
+import Sans
+
+while True:
+    text = input('Sans> ')
+    result, error = Sans.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    elif result:
+        print(result)
+
+# committed'''
+import Run
+import codecs
+
+'''with codecs.open("D:\\pro.txt", encoding='utf-8') as p:
+    inp = p.read()
+    print(inp)
+    result, error = Run.run('program.txt', inp)
+
+    if error:
+        print(error.as_string())
+    else:
+        print(result)'''
+
